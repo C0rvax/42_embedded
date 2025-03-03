7561cbe2ec4128e8fdc59317fac3127538fe4265
aduvilla <aduvilla@sudent.42.fr>
2025-03-03 23:57:44 +0100

    mieux makefile

--- Module00/ex01/main.c
+++ Module00/ex01/main.c
@@ -2,7 +2,7 @@
 
 int	main()
 {
-	// DDRX (Data Direction Register) define if the pin is in (0) or out (1)
+	// DDRX (Data Direction Register) define if the pin in port X is in (0) or out (1) (Direction)
 	DDRB |= (1 << PB0); // set PB0 (port B, pin 0) to out (1) (1 << PB0) shift a bit of PB0 (0)
 	// PORTX (Data Register) set level logic high (1) or low (0) for out and set pull-up resistance high (1) or low (0) for in
 	PORTB |= (1 << PB0); // set PBO to high (1) turn on the LED
