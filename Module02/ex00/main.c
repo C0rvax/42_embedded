b33f84d934bdd1250da1182749719ed8080fcb8d
aduvilla <aduvilla@student.42.fr>
2025-03-04 16:38:29 +0100

    debut module 2

--- Module02/ex00/main.c
+++ Module02/ex00/main.c
@@ -1,53 +1,31 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#define UART_BAUDRATE 115200
+#define F_CPU 16000000UL
+#define MYUBBR (F_CPU / (16 * UART_BAUDRATE))-1
+
+void	uart_tx(char c)
+{
+	while (!(UCSR0A & (1 << UDRE0))); // wait for empty buffer
+	UDR0 = c; // send char
+}
+
+void	uart_init(unsigned int ubbr)
+{	
+	UBRR0L = (unsigned char)ubbr;
+	UBRR0H = (unsigned char)(ubbr >> 8); // configure Baud rate
+	
+	UCSR0B = (1 << TXEN0); // activate TX (transmission)
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // Mode 8N1 (8bits, no parity, 1 stop bit)
+}
 
 int	main()
 {
-	DDRB |= (1 << PB1); // set PB1 to out (1)
-	DDRD &= ~((1 << PD2) | (1 << PD4)); // set PD2 and PD4 to in (1)
-	PORTD |= (1 << PD2) | (1 << PD3); // set pull-up res for PD2 and PD4
-
-	// configure Timer1 in mode Fast PWM (Mode 14) with ICR1 for TOP
-	TCCR1A |= (1 << COM1A1); // Mode PWM, OC1A "Clear on compare Match" (page 109)
-	TCCR1A |= (1 << WGM11); // Mode Fast PWM with ICR1 TOP and OCR1A at BOTTOM (mode 14, page 109)
-	TCCR1B |= (1 << WGM12) | (1 << WGM13); // Mode Fast PWM with ICR1 and OCR1A at BOTTOM (mode 14, page 109)
-	TCCR1B |= (1 << CS12) | (1 << CS10); // with a prescaler of 1024 (page 110)
-	
-	ICR1 = 15625; // set TOP = 1s (15625 * 1024 = 16 Hz)
-
-	uint16_t tCycle = 1562; // 10% of ICR1
-	OCR1A = tCycle;
-
-	uint8_t prevSw1State = 1;
-	uint8_t prevSw2State = 1;
-
+	uart_init(MYUBBR);
 	while (1)
 	{
-		uint8_t sw1State = PIND & (1 << PD2);
-		uint8_t sw2State = PIND & (1 << PD4);
-
-		if (!sw1State && prevSw1State)
-		{
-			_delay_ms(50);
-			if (!(PIND & (1 << PD2)))
-			{
-				if (tCycle <= 14062) // max 90%
-					tCycle += 1562;
-			}
-		}
-		if (!sw2State && prevSw2State)
-		{
-			if (!(PIND & (1 << PD4)))
-			{
-				_delay_ms(50);
-				if (tCycle >= 1562) // min 10%
-					tCycle -= 1562;
-			}
-		}
-		OCR1A = tCycle;
-		prevSw1State = sw1State;
-		prevSw2State = sw2State;
+		uart_tx('Z');
+		_delay_ms(1000);
 	}
-
 	return 0;
 }
