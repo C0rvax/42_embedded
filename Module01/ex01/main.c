20db61f60f75ff3829604a44d711feaf343cb007
aduvilla <aduvilla@student.42.fr>
2025-03-05 11:58:50 +0100

    en phase correct

--- Module01/ex01/main.c
+++ Module01/ex01/main.c
@@ -6,8 +6,10 @@
 // OCRx Output Compare Register
 // TCNTx Timer/Counter
 // COMx Compare Output Mode
+// 1s = 16000000 / 256 (prescaler) = 62500
 int	main()
 {
+	/*
 	DDRB |= (1 << PB1); // set PB1 to out (1)
 
 	TCCR1B |= (1 << WGM12); // Mode CTC (Clear Timer on Compare Match) (page 109)
@@ -15,12 +17,14 @@ int	main()
 	TCCR1A |= (1 << COM1A0); // define toggle mode on OC1A (page 108)
 	
 	OCR1A = 7812; // comparare value for 500ms (16000000 / 1024 / 2)
-	
+*/	
+	DDRB |= (1 << PB1); // set PB1 to out (1)
+	TCCR1B |= (1 << WGM13); // mode 10 PWM phase correct ICR1 TOP
+	TCCR1A |= (1 << WGM11); // mode 10 PWM phase correct ICR1 TOP
+	TCCR1B |= (1 << CS12); // with a prescaler of 256 (page 110)
+	TCCR1A |= (1 << COM1A1); // clear at match up-counting / set down-couting
+	OCR1A = 62500 / 4;
+	ICR1 = 62500 / 2;
 	while (1);
 	return 0;
 }
-/*
-	TCCR1B |= (1 << WGM13);
-	TCCR1A |= (1 << WGM10); // mode 9 PWM phase and freq correct
-	TCCR1A |= (1 << COM1A1); // clear at TOP / set at BOTTOM
-*/
--- Module01/ex02/main.c
+++ Module01/ex02/main.c
@@ -6,8 +6,18 @@
 // OCRx Output Compare Register
 // TCNTx Timer/Counter
 // COMx Compare Output Mode
+// 1s = 16000000 / 256 (prescaler) = 62500
 int	main()
 {
+	DDRB |= (1 << PB1); // set PB1 to out (1)
+	TCCR1B |= (1 << WGM13); // mode 10 PWM phase correct ICR1 TOP
+	TCCR1A |= (1 << WGM11); // mode 10 PWM phase correct ICR1 TOP
+	TCCR1B |= (1 << CS12); // with a prescaler of 256 (page 110)
+	TCCR1A |= (1 << COM1A1); // clear at match up-counting / set down-couting
+	OCR1A = 62500 / 20;
+	ICR1 = 62500 / 2;
+	while (1);
+	/*
 	DDRB |= (1 << PB1); // set PB1 to out (1)
 
 	// configure Timer1 in mode Fast PWM (Mode 14) with ICR1 for TOP
@@ -18,6 +28,7 @@ int	main()
 	
 	ICR1 = 15625; // set TOP = 1s (15625 * 1024 = 16 M)
 	OCR1A = 1562; // set Compare match at 100ms (10% of TOP)
+	*/
 	
 	return 0;
 }
