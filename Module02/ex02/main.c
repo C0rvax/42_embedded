6e567099445d442c7f318e45bc3c8c61939297b9
aduvilla <aduvilla@student.42.fr>
2025-03-05 18:52:27 +0100

    debut ex02

---
+++ Module02/ex02/main.c
@@ -0,0 +1,50 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#define UART_BAUDRATE 115200
+#define F_CPU 16000000UL
+#define MYUBRR ((unsigned char)((F_CPU / (16.0 * UART_BAUDRATE)) - 0.5)) // Table 19-12 (p.165)
+
+void	uart_tx(char c)
+{
+	while (!(UCSR0A & (1 << UDRE0))); // wait for empty buffer
+	UDR0 = c; // send char
+}
+
+void	uart_tx_string(const char *str)
+{
+	while (*str)
+		uart_tx(*str++);
+}
+
+void	uart_init(unsigned char ubrr)
+{	
+	UBRR0L = (unsigned char)ubrr;
+	UBRR0H = (unsigned char)(ubrr >> 8); // configure Baud rate
+	
+	UCSR0B = (1 << TXEN0); // activate TX (transmission)
+	UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00); // Mode 8N1 (8bits, no parity, 1 stop bit)
+}
+
+void	timer1_init()
+{
+	TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10);
+	OCR1A = ((F_CPU / 1024) * 2) - 1;
+	TIMSK1 |= (1 << OCIE1A); // activate interrupt
+	sei();
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	uart_tx_string("Hello World!\r\n");
+}
+
+int	main(void)
+{
+	uart_init(MYUBRR);
+//	_delay_ms(100);
+	timer1_init();
+
+	while (1);
+	return 0;
+}
