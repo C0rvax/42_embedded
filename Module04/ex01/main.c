a3363fc1f8a4b59adda2425e165ec450b5fc30aa
aduvilla <aduvilla@student.42.fr>
2025-03-07 14:51:28 +0100

    le 01 est bon

--- Module04/ex01/main.c
+++ Module04/ex01/main.c
@@ -1,18 +1,24 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdint.h>
-#define TMAX 62500
+#define TMAX 80
 
 volatile uint16_t cycle = 0;
-volatile int8_t step = 5;
+volatile int8_t step = 1;
 
 void	init_timers(void)
 {
 	// ** Config Timer0
 	TCCR0A = 0; // Normal Mode
-	TCCR0B |= (1 << CS00); // Prescaler 1
+	TCCR0B |= (1 << CS00) | (1 << CS02); // Prescaler 1024
 	TIMSK0 |= (1 << TOIE0); // overflow interrupt every 1 (Prescaler) * 256 (TOP 0xFF) ticks (62500 in a sec)
 
+
+	// ** Config Timer1
+	TCCR1A |= (1 << COM1A1) | (1 << WGM11); // Fast PWM TOP ICR1
+	TCCR1B |= (1 << WGM10) | (1 << WGM13)| (1 << CS10); // Prescaler 1
+	ICR1 = TMAX;
+	OCR1A = cycle;
 
 	DDRB |= (1 << PB1);
 
@@ -21,13 +27,7 @@ void	init_timers(void)
 
 ISR(TIMER0_OVF_vect)
 {
-	// ** Config Timer1
-	TCCR1A |= (1 << COM1A1) | (1 << WGM11); // Fast PWM TOP ICR1
-	TCCR1B |= (1 << WGM12) | (1 << WGM13)| (1 << CS10); // Prescaler 1
-	ICR1 = TMAX;
-	OCR1A = cycle;
 
-//	TCNT1 = 0;
 	cycle += step;
 	if (cycle == TMAX || cycle == 0)
 		step = -step; // reverse
