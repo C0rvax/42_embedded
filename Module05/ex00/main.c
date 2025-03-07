2627fe170b7909f37da0216e5e0485423e2b5866
aduvilla <aduvilla@sudent.42.fr>
2025-03-07 17:52:07 +0100

    mkdir mod5

---
+++ Module05/ex00/main.c
@@ -0,0 +1,42 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#define TMAX 61
+
+volatile uint16_t cycle = 0;
+volatile int8_t step = 1;
+
+void	init_timers(void)
+{
+	// ** Config Timer0
+	TCCR0A = 0; // Normal Mode
+	TCCR0B |= (1 << CS00) | (1 << CS02); // Prescaler 1024
+	TIMSK0 |= (1 << TOIE0); // overflow interrupt every 1 (Prescaler) * 256 (TOP 0xFF) ticks (62500 in a sec)
+
+
+	// ** Config Timer1
+	TCCR1A |= (1 << COM1A1) | (1 << WGM11); // Fast PWM TOP ICR1
+	TCCR1B |= (1 << WGM10) | (1 << WGM13)| (1 << CS10); // Prescaler 1
+	ICR1 = TMAX;
+	OCR1A = cycle;
+
+	DDRB |= (1 << PB1);
+
+	sei();
+}
+
+ISR(TIMER0_OVF_vect)
+{
+
+	cycle += step;
+	if (cycle == TMAX || cycle == 0)
+		step = -step; // reverse
+
+	OCR1A = cycle;
+}
+
+int	main(void)
+{
+	init_timers();
+	while (1);
+}
