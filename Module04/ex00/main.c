7611a1cbc8e0a5891a31017d7749b10fcbd1ad49
aduvilla <aduvilla@student.42.fr>
2025-03-10 16:31:02 +0100

    module 04 ok

--- Module04/ex00/main.c
+++ Module04/ex00/main.c
@@ -2,16 +2,6 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-void start_debounce_timer()
-{
-    // Configure Timer0 on mode CTC (Comparaison)
-    TCCR0A = (1 << WGM01);  // Mode CTC
-    TCCR0B = (1 << CS02) | (1 << CS00);  // Prescaler 1024
-	// OCR0A + 1 = delay x F_CPU / prescaler
-    OCR0A = 233;  // for delay = 15ms -> OCR0A = 0.020 * 16000000 / 1024 -1 = 232.375 
-    TIMSK0 |= (1 << OCIE0A);  // Activate interrupt of Timer0 comp A
-}
-
 void	init_setup(void)
 {
 	DDRB |= (1 << PB0); // LED out (1)
@@ -23,19 +13,11 @@ void	init_setup(void)
 	sei();
 }
 
-ISR(TIMER0_COMPA_vect)
-{
-	TIMSK0 &= ~(1 << OCIE0A); // Disable interrupt Timer0
-	TCCR0B = 0; // Stop timer
-	PORTB ^= (1 << PB0);
-	EIMSK |= (1 << INT0); // Reset INT0
-}
-
 ISR(INT0_vect)
 {
-	EIMSK &= ~(1 << INT0); // Disable interrupt
+//	EIMSK &= ~(1 << INT0); // Disable interrupt
 	_delay_ms(50);
-	start_debounce_timer();
+	PORTB ^= (1 << PB0);
 }
 
 int	main(void)
@@ -43,3 +25,21 @@ int	main(void)
 	init_setup();
 	while (1);
 }
+/*
+ISR(TIMER0_COMPA_vect)
+{
+	TIMSK0 &= ~(1 << OCIE0A); // Disable interrupt Timer0
+	TCCR0B = 0; // Stop timer
+	EIMSK |= (1 << INT0); // Reset INT0
+}
+
+void start_debounce_timer()
+{
+    // Configure Timer0 on mode CTC (Comparaison)
+    TCCR0A = (1 << WGM01);  // Mode CTC
+    TCCR0B = (1 << CS02) | (1 << CS00);  // Prescaler 1024
+	// OCR0A + 1 = delay x F_CPU / prescaler
+    OCR0A = 233;  // for delay = 15ms -> OCR0A = 0.020 * 16000000 / 1024 -1 = 232.375 
+    TIMSK0 |= (1 << OCIE0A);  // Activate interrupt of Timer0 comp A
+}
+*/
