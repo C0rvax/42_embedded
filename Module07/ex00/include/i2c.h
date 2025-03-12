01d378be4935d5ed93f4010f0e2af882ecebd20d
aduvilla <aduvilla@student.42.fr>
2025-03-12 17:47:22 +0100

    debut du 07

---
+++ Module07/ex00/include/i2c.h
@@ -0,0 +1,35 @@
+#ifndef I2C_HPP
+# define I2C_HPP
+
+# include <avr/io.h>
+#include <stdint.h>
+
+
+/*
+* TWI Two Wire service Interface
+* TWBR TW Baud Rate
+* TWSR TW Status Register
+* TWCR TW Control Register
+* TWEN TW ENable
+* TWSTA TW STArt
+* TWSTO TW STOp
+* TWINT TW INTerrupt flag
+* TWDR TW Data Register
+* TWAR TW Address Register
+* Write (low) 0
+* Read (high) 1
+*/
+
+#define F_CPU 16000000UL  // Freq mcu
+#define F_SCL 100000UL    // Freq I2C (100 kHz)
+#define TWBR_VALUE ((F_CPU / F_SCL - 16) / 2)  // (p.180)
+#define AH20_ADDRESS 0x38
+
+void	i2c_init(void);
+void	i2c_start(void);
+void	i2c_write( uint8_t data);
+void	i2c_stop(void);
+uint8_t	i2c_read(uint8_t ack);
+void	i2c_write_status(const char* prompt);
+
+#endif /* __I2C_HPP__ */
---
+++ Module07/ex00/src/prog.c
@@ -0,0 +1,37 @@
+#include "prog.h"
+
+void uart_init(void)
+{
+    UBRR0H = (uint8_t)(UBRR_VALUE >> 8); // Configure the UBRR0H register (see section 20.11.1 of the doc)
+    UBRR0L = (uint8_t)UBRR_VALUE; // Configure the UBRR0L register
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0); // Enable reception and transmission (see section 20.11.2 of the doc)
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // Configure the frame format (8 bits) (see section 20.11.3 of the doc)
+}
+
+void uart_tx(char c)
+{
+    while (!(UCSR0A & (1 << UDRE0))); // Wait until the register is ready (see section 20.11.2 of the doc)
+    UDR0 = c; // Send the character
+}
+
+void uart_tx_string(const char *str)
+{
+    while (*str)
+        uart_tx(*str++); // Send each character of the string
+}
+
+char uart_rx(void)
+{
+    while (!(UCSR0A & (1 << RXC0))); // Wait for a character to be received (see section 20.11.3 of the doc)
+    return UDR0; // Return the received character
+}
+
+void	uart_print_status(uint8_t status)
+{
+	const char hex_digits[] = "0123456789ABCDEF";
+//	uart_tx_string("0x");
+	uart_tx(hex_digits[(status >> 4) & 0x0F]);
+	uart_tx(hex_digits[status & 0x0F]);
+	uart_tx(' ');
+//	uart_tx_string("\r\n");
+}
