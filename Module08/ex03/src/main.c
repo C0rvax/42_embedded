7d426cea3dc9ed64f8a09ce3221745222062510e
aduvilla <aduvilla@student.42.fr>
2025-03-13 13:00:12 +0100

    all good

--- Module08/ex03/src/main.c
+++ Module08/ex03/src/main.c
@@ -1,21 +1,33 @@
 #include "spi.h"
+#include "adc.h"
+
+void	set_led(uint8_t rv1)
+{
+	switch (rv1 / 85)
+	{
+		case 3:
+			APA102(2, 31, 255, 255, 255);
+		case 2:
+			APA102(1, 31, 255, 255, 255);
+		case 1:
+			APA102(0, 31, 255, 255, 255);
+			break;
+		case 0:
+			APA102(4, 31, 255, 255, 255);
+	}
+}
 
 int main(void)
 {
     // Initialize SPI as master
     SPI_MasterInit();
+	adc_init();
+	uint8_t rv1;
 
     while (1)
     {
+		rv1 = adc_read(0);
+		set_led(rv1);
         // Turn on LED D6 connected to MOSI (PB3)
-        _delay_ms(250);
-		APA102(0, 31, 255, 255, 255);
-        _delay_ms(250);
-		APA102(1, 31, 255, 255, 255);
-        _delay_ms(250);
-		APA102(2, 31, 255, 255, 255);
-        _delay_ms(250);
-		APA102(4, 31, 0, 0, 0);
-        _delay_ms(250);
     }
 }
