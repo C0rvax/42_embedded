f2690d3d73e2885f1428cd58214bbd8d7125db34
aduvilla <aduvilla@student.42.fr>
2025-03-15 12:49:51 +0100

    debut du 09-06

---
+++ Module09/ex06/src/main.c
@@ -0,0 +1,38 @@
+#include "i2c.h"
+#include "seven.h"
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+volatile uint8_t timerOk = 0;
+
+void	init_timer(void)
+{
+	TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10); // CTC prescaler 1024
+	OCR1A = 15624; // 30Hz
+	TIMSK1 |= (1 << OCIE1A);
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	timerOk = 1;
+	TCCR1B &= ~((1 << CS12) | (1 << CS10));
+}
+
+int main(void)
+{
+    i2c_init();   // Initialisation I2C
+    init_segments();  // Configuration des segments en sortie
+    init_digits(); // Configuration des digits en sortie
+	sei();
+
+	uint16_t number = 0;
+    while (1)
+	{
+		timerOk = 0;
+		init_timer();
+		while (!timerOk)
+			display_number(number);
+//		_delay_ms(1000);
+		number = (number + 1) % 10000;
+	}
+}
